d05b486a26bb808bb3ad818e0e687e474f7c62e4
sonkadak <45662574+sonkadak@users.noreply.github.com>
2019-04-07 16:05:20 +0900

    Still not understood clearly

--- Hackerrank/Interview-preparation-kit/Arrays/New-Year-Chaos.py
+++ Hackerrank/Interview-preparation-kit/Arrays/New-Year-Chaos.py
@@ -4,13 +4,15 @@
 # Complete the minimumBribes function below.
 #def minimumBribes(q):
 def minimumBribes(a):
-    cnt = 0
-    for i in a:
-        if (i-1) - (a.index(i)) > 2:
-            return print ("Too chaotic")
-        elif (i-1) - (a.index(i)) == 2:
-            cnt += 2
-        elif (i-1) - (a.index(i)) == 1:
-            cnt += 1
+    moves = 0
+    for i, p in enumerate(q):
+        #  현재 위치(i), 원래 위치(p) 비교
+        if p - (i + 1) > 2:
+            print("Too chaotic")
+            return
+        # 비교 대상(처음 사람의 원래 위치 부터 현재 위치까지)와 원래 위치(p)와 비교하여 이동이 있을 경우 카운트
+        for j in range(max(p-2, 0), i):
+            if q[j] > p:
+                moves += 1
             
-    return print (cnt)
+    return print (moves)
